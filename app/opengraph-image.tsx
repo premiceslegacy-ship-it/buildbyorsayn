969e1fab92e60ee4c14c3a28639b71a7bc31616e
Samuel Mbeboura <useersm@MacBook-Air-de-SM.local>
2026-04-12 21:58:57 +0200

    fix: directly scale SVG paths inside group to bypass Next.js OG viewBox bugs

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -24,33 +24,36 @@ export default async function Image() {
         }}
       >
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyItems: 'center', gap: '30px' }}>
-          <div style={{ display: 'flex' }}>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <svg 
-              width="450" 
-              height="350" 
-              viewBox="-5 25 200 165" 
+              width="500" 
+              height="400" 
+              viewBox="0 0 500 400" 
               fill="none" 
               xmlns="http://www.w3.org/2000/svg"
             >
-              <g transform="translate(0, 55)">
-                <path fill="#e8d5b0" d="M50 25 L100 0 L150 25 L100 50 Z"></path>
-                <path fill="#30261c" d="M50 25 L100 50 V110 L50 85 Z"></path>
-                <path fill="#c9b48a" d="M150 25 L100 50 V110 L150 85 Z"></path>
-              </g>
-              <g transform="translate(10, 80)">
-                <path fill="#e8d5b0" d="M0 25 L45 2.5 L90 25 L45 47.5 Z"></path> 
-                <path fill="#30261c" d="M0 25 L45 47.5 V100 L0 77.5 Z"></path> 
-                <path fill="#c9b48a" d="M90 25 L45 47.5 V100 L90 77.5 Z"></path> 
-              </g>
-              <g transform="translate(55, 57)">
-                <path fill="#e8d5b0" d="M0 25 L45 2.5 L90 25 L45 47.5 Z"></path>
-                <path fill="#30261c" d="M0 25 L45 47.5 V100 L0 77.5 Z"></path>
-                <path fill="#c9b48a" d="M90 25 L45 47.5 V100 L90 77.5 Z"></path> 
-              </g>
-              <g transform="translate(100, 34)">
-                <path fill="#e8d5b0" d="M0 25 L45 2.5 L90 25 L45 47.5 Z"></path>
-                <path fill="#30261c" d="M0 25 L45 47.5 V100 L0 77.5 Z"></path>
-                <path fill="#c9b48a" d="M90 25 L45 47.5 V100 L90 77.5 Z"></path>
+              {/* Le logo original fait ~180x145. On le multiplie par 2.5 (-> 450x360) et on le centre */}
+              <g transform="translate(25, 20) scale(2.5)">
+                <g transform="translate(0, 55)">
+                  <path fill="#e8d5b0" d="M50 25 L100 0 L150 25 L100 50 Z"></path>
+                  <path fill="#30261c" d="M50 25 L100 50 V110 L50 85 Z"></path>
+                  <path fill="#c9b48a" d="M150 25 L100 50 V110 L150 85 Z"></path>
+                </g>
+                <g transform="translate(10, 80)">
+                  <path fill="#e8d5b0" d="M0 25 L45 2.5 L90 25 L45 47.5 Z"></path> 
+                  <path fill="#30261c" d="M0 25 L45 47.5 V100 L0 77.5 Z"></path> 
+                  <path fill="#c9b48a" d="M90 25 L45 47.5 V100 L90 77.5 Z"></path> 
+                </g>
+                <g transform="translate(55, 57)">
+                  <path fill="#e8d5b0" d="M0 25 L45 2.5 L90 25 L45 47.5 Z"></path>
+                  <path fill="#30261c" d="M0 25 L45 47.5 V100 L0 77.5 Z"></path>
+                  <path fill="#c9b48a" d="M90 25 L45 47.5 V100 L90 77.5 Z"></path> 
+                </g>
+                <g transform="translate(100, 34)">
+                  <path fill="#e8d5b0" d="M0 25 L45 2.5 L90 25 L45 47.5 Z"></path>
+                  <path fill="#30261c" d="M0 25 L45 47.5 V100 L0 77.5 Z"></path>
+                  <path fill="#c9b48a" d="M90 25 L45 47.5 V100 L90 77.5 Z"></path>
+                </g>
               </g>
             </svg>
           </div>
@@ -58,11 +61,11 @@ export default async function Image() {
           <div
             style={{
               display: 'flex',
-              fontSize: '85px',
+              fontSize: '110px',
               fontWeight: 800,
               color: '#f0ede8',
-              letterSpacing: '14px',
-              marginTop: '20px',
+              letterSpacing: '24px',
+              marginTop: '40px',
               textTransform: 'uppercase',
             }}
           >
